6ae3e99de49b059b2c933f0d8c6eb7516a8765a2
Ubuntu <ubuntu@ip-172-31-12-16.ap-northeast-2.compute.internal>
2024-10-06 05:00:01 +0000

    deployment/replicaset

---
+++ kops_5_deployments.sh
@@ -0,0 +1,14 @@
+# commands
+kubectl get deployments
+kubectl get rs # not rc
+kubectl get pods --show-labels # 파드에 레이블 추가
+kubectl rollout status deployment/{배포객체명} # get status
+# 최신버전 업데이트
+kubectl set image deployment/{배포객체명} k8s-demo=k8s-demo:2
+kubectl edit deployment/{배포객체명}
+# 새 버전으로 업데이트된 이미지의 배포 상태 확인
+kubectl rollout status deployment/{배포객체명} # status of rollout
+# get history
+kubectl rollout history deployment/{배포객체명}
+# rollback
+kubectl rollout undo deployment/{배포객체명} --to-revision=n
