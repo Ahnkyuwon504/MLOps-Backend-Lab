a0597d3727a2d5342017fe8da9b2b347c001dfe4
Ubuntu <ubuntu@ip-172-31-12-16.ap-northeast-2.compute.internal>
2024-10-07 14:15:39 +0000

    healthcheck

---
+++ kops_8_healthcheck.sh
@@ -0,0 +1,16 @@
+cat deployment/helloworld-healthcheck.yml
+
+# 배포 생성
+kubectl create -f deployment/helloworld-healthcheck.yml
+
+# 확인
+kubectl get pods
+kubectl describe pods {pod name}
+    Liveness: http-get http://:nodejs-port/ delay=15s timeout=30s period=10s #success=1 #failure=3
+
+# 오류시 파드 종료/재시작
+# 비정상 파드로 트래픽을 라우팅하지 않음. 프로덕션 필수
+
+# 편집
+kubectl edit
+kubectl edit deployment/helloworld-deployment
